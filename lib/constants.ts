4b8e879737a22ee1174cbd74e365b8d5cc71e5d7
Dominick Bruno <dombruno93@gmail.com>
2021-01-13 23:43:12 -0500

    Update constants.ts
    testing

--- lib/constants.ts
+++ lib/constants.ts
@@ -17,46 +17,34 @@
 export const SITE_URL = 'https://demo.vercel.events';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const BRAND_NAME = '2nd Global Virtual Agile in Education Conference';
+export const SITE_NAME_MULTILINE = ['2nd Global Virtual', 'Agile in Education Conference'];
+export const SITE_NAME = '2nd Global Virtual Agile in Education Conference';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'Agile in Education: Transformation Education through Innovative Solutions';
 export const SITE_DESCRIPTION =
-  'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
-export const SHORT_DATE = 'Oct 27 - 9:00am PST';
-export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
+  'Agile in Education: Transformation Education through Innovative Solutions';
+export const DATE = '18-20 March 2021';
+export const SHORT_DATE = '18-20 March - 7-11pm BDT';
+export const FULL_DATE = '18-20 March 7-11pm Bangladesh Time (GMT+6)';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
 // Remove process.env.NEXT_PUBLIC_... below and replace them with
 // strings containing your own privacy policy URL and copyright holder name
 export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
-export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
+export const COPYRIGHT_HOLDER = '2021 Global Agile in Education Conference';
 
 export const CODE_OF_CONDUCT =
   'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
-export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
+export const REPO = 'https://github.com/DomBruno/virtual-event-starter-kit-test';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
     name: 'Stage A',
     route: '/stage/a'
   },
-  {
-    name: 'Stage C',
-    route: '/stage/c'
-  },
-  {
-    name: 'Stage M',
-    route: '/stage/m'
-  },
-  {
-    name: 'Stage E',
-    route: '/stage/e'
-  },
-  {
+   {
     name: 'Schedule',
     route: '/schedule'
   },
@@ -67,10 +55,6 @@ export const NAVIGATION = [
   {
     name: 'Expo',
     route: '/expo'
-  },
-  {
-    name: 'Jobs',
-    route: '/jobs'
   }
 ];
 
