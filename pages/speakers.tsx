93704df6077e4768362f14d5b0b57ef323d353ca
Dominick Bruno <dombruno93@gmail.com>
2021-01-15 06:21:12 -0500

    Update speakers.tsx
    Update title to Conference

--- pages/speakers.tsx
+++ pages/speakers.tsx
@@ -31,7 +31,7 @@ type Props = {
 
 export default function Speakers({ speakers }: Props) {
   const meta = {
-    title: 'Speakers - Virtual Event Starter Kit',
+    title: 'Speakers - Global Agile in Education Conference',
     description: META_DESCRIPTION
   };
   return (
